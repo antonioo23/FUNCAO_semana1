46b2ae389208ab3f78a9b1e75878fb4e035583d5
Antonio <107552515+antonioo23@users.noreply.github.com>
2024-06-07 08:26:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-aesultados");
+const ctextoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Perguntas 1",
+        alternativas: [
+            "Alternativa 1,"
+            "Alternativa 2"
+        ]
+    },
+
+    {
+        enunciado: "Perguntas 2",
+        alternativas: [
+            "Alternativa 1,"
+            "Alternativa 2"
+        ]
+    }
+
+];
